65fe5d71cef90fbb1b22e8990ce46b3e1137461a
ManuDev03 <manuprajan03@gmail.com>
2021-08-22 10:38:29 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+
+app.get('/',(req,res) => {
+    res.send('express app is running')
+})
+app.listen(port, () => {console.log('server is runing on port' + port);})
